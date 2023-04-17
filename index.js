86d4cf67fd59456eb3f3c4bce9130786b669f8c1
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2023-04-17 14:40:02 +0200

    refactoring JS

--- index.js
+++ index.js
@@ -79,28 +79,35 @@ let text = [
 ];
 
 const lengthEl = document.getElementById("length-el");
+const passwordsContainer = document.getElementById("passwords-container");
+const genBtn = document.getElementById("gen-Btn");
+let password;
 
-function generate() {
-  const pwEl1 = document.getElementById("password1-el");
-  pwEl1.value = getPassword();
-  const pwEl2 = document.getElementById("password2-el");
-  pwEl2.value = getPassword();
-  const pwEl3 = document.getElementById("password3-el");
-  pwEl3.value = getPassword();
-  const pwEl4 = document.getElementById("password4-el");
-  pwEl4.value = getPassword();
-  document.querySelector(".passwords").classList.remove("dots");
-}
-
-function getPassword() {
-  let password = "";
-  let passwordLength = 0;
-  passwordLength = document.getElementById("length-el").value;
+function generatePassword() {
+  const passwordLength = lengthEl.value;
+  password = "";
 
   for (let i = 0; i < passwordLength; i++) {
     let randomChar = text[Math.floor(Math.random() * text.length)];
-
     password += randomChar;
   }
   return password;
 }
+function getPasswordArray() {
+  return new Array(4).fill("0").map(function () {
+    return generatePassword();
+  });
+}
+function getPasswordHtml() {
+  return getPasswordArray().map(
+    (item) => ` <input autocomplete="off" type="text" value="${item}" />`
+  );
+}
+
+function render() {
+  document.querySelector(".passwords").classList.remove("dots");
+  passwordsContainer.innerHTML = getPasswordHtml();
+  lengthEl.value = "";
+}
+
+genBtn.addEventListener("click", render);
